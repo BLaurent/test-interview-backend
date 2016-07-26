37bab419048d2b8d1c4730a23cae873aa5afd7e5
100026625 <benoit.laurent@ge.com>
2016-07-26 09:46:47 +0200

    Update pom artifact and jersey config (cherry picked from commit 9ffc230)

--- src/main/java/com/ge/digital/JerseyConfig.java
+++ src/main/java/com/ge/digital/JerseyConfig.java
@@ -14,10 +14,13 @@ package com.ge.digital;/*
  *
  */
 
+import javax.ws.rs.ApplicationPath;
+
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ApplicationPath("/")
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         this.packages("com.ge.digital.endpoints");
