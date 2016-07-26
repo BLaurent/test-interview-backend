bca99460eca2958e5d9762af234802db93124029
100026625 <Benoit.Laurent@ge.com>
2016-07-26 08:41:11 +0200

    second commit

---
+++ src/main/java/com/ge/digital/JerseyConfig.java
@@ -0,0 +1,25 @@
+/********************************************************************************
+ * Copyright (c) 2015-2016 GE Digital. All rights reserved.                     *
+ *                                                                              *
+ * The copyright to the computer software herein is the property of GE Digital. *
+ * The software may be used and/or copied only with the written permission of   *
+ * GE Digital or in accordance with the terms and conditions stipulated in the  *
+ * agreement/contract under which the software has been supplied.               *
+ ********************************************************************************/
+
+package com.ge.digital;/*
+ * test-interview-backend 
+ * 
+ * Created by benoitlaurent on 25/07/16
+ *
+ */
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig() {
+        this.packages("com.ge.digital.endpoints");
+    }
+}
